dbd2458bc11d3537f70e859bc004677e9517e6bb
MattRig7 <mattrigo2872@gmail.com>
2023-05-26 17:11:48 +1000

    added remaining test cases
    Signed-off-by: MattRig7 <mattrigo2872@gmail.com>

--- testCasesM3/runTestCasesM3.sh
+++ testCasesM3/runTestCasesM3.sh
@@ -3,16 +3,26 @@
 cd ..
 make
 
-
+#The below REQ tests are used to evaluate the performance of the doubly-linked list (major enhancement)
 #REQ2 TESTS
 ./ppd stock.dat coins.dat < ./testCasesM3/ppdTestREQ2.input > ./testCasesM3/ppdTestREQ2.actual_ppd_out
 diff -w ./testCasesM3/ppdTestREQ2.output ./testCasesM3/ppdTestREQ2.actual_ppd_out
 diff -w -y ./testCasesM3/ppdTestREQ2.expcoins ./coins.dat
 
-#REQ3 TESTS
+# #REQ3 TESTS
 ./ppd stock.dat coins.dat < ./testCasesM3/ppdTestREQ3.input > ./testCasesM3/ppdTestREQ3.actual_ppd_out
 diff -w ./testCasesM3/ppdTestREQ3.output ./testCasesM3/ppdTestREQ3.actual_ppd_out
 diff -w -y ./testCasesM3/ppdTestREQ3.expcoins ./coins.dat
+
+#REQ7 TESTS
+./ppd stock.dat coins.dat < ./testCasesM3/ppdTestREQ7.input > ./testCasesM3/ppdTestREQ7.actual_ppd_out
+diff -w ./testCasesM3/ppdTestREQ7.output ./testCasesM3/ppdTestREQ7.actual_ppd_out
+diff -w -y ./testCasesM3/ppdTestREQ7.expcoins ./coins.dat
+
+# #REQ8 TESTS
+./ppd stock.dat coins.dat < ./testCasesM3/ppdTestREQ8.input > ./testCasesM3/ppdTestREQ8.actual_ppd_out
+diff -w ./testCasesM3/ppdTestREQ8.output ./testCasesM3/ppdTestREQ8.actual_ppd_out
+diff -w -y ./testCasesM3/ppdTestREQ8.expcoins ./coins.dat
 
 #Invalid Input TESTS
 ./ppd stock.dat coins.dat < ./testCasesM3/ppdTestInvalidInput.input > ./testCasesM3/ppdTestInvalidInput.actual_ppd_out
